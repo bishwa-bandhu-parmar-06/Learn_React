02c9d2c360ef19b36c3d14b4e863f1e3763506f7
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2024-11-23 20:01:44 +0530

    learning Installation

---
+++ vite-project1/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import "bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ vite-project1/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+        <h1>Todo App</h1>
+        
+    </>
+  )
+}
+
+export default Hero
